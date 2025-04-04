c090638c5dcca5942fb5f07e813796089e3511ca
DavideMascheroni1 <97758477+DavideMascheroni1@users.noreply.github.com>
2025-04-04 19:03:56 +0200

    Adding red

--- stimoli.py
+++ stimoli.py
@@ -13,11 +13,12 @@ import random
 # Dialogue window for the tester number
 application_window = tkinter.Tk()
 testernumber = simpledialog.askstring("Input", "Input tester number", parent=application_window)
-#Insert the trial number
+#Insert the trial number of an user
 index = simpledialog.askstring("Input", "Input trial number", parent=application_window)
 
 # Color definitions
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 #Window creation
 winsize = (sizeWidth, sizeHeight) = (1280, 720)

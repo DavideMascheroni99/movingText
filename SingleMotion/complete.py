3c0d555f56089712d12223f7c4c714b69c92a14f
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-04-10 12:49:30 +0200

    verticalBox + minor changes

--- SingleMotion/complete.py
+++ SingleMotion/complete.py
@@ -68,6 +68,24 @@ def prepareVerText(text, font, x, y):
     screen.blit(img, (x, y))
     pygame.display.flip()
 
+#add # every n characters
+def addSeparator(text, n):
+  s = list(text)
+  for i in range(len(text)):
+    if(i%n == 0) and i != 0:
+      if (s[i] == ' '):
+        del s[i]
+      s[i] = '#' + s[i]
+ 
+  text = ''.join(s)
+  return text
+
+def createVertBlock(x, y, font, nlText):
+  for i in range(len(nlText)):
+    img = font.render(nlText[i], True, WHITE)
+    screen.blit(img, (x, y))
+    y = y + 45
+
 
 #Box text horizontal move
 def verticalMove():
@@ -75,7 +93,7 @@ def verticalMove():
   #Starting image position
   x = (sizeWidth / 2) - (300/2)
   y = 0
-  speed = 0.2
+  speed = 0.1
 
   #Load and rescale the text image
   #picture = pygame.image.load('movingText/Images/text2.png').convert()
@@ -209,7 +227,7 @@ def diagMove():
   #Starting image position
   x = 0
   y = 0
-  speed = 0.2
+  speed = 0.1
 
   #Load and rescale the text image
   #picture = pygame.image.load('movingText/Images/text3.png').convert()
@@ -279,7 +297,7 @@ def horizontalScroll():
   #Create a font
   font = pygame.font.SysFont("Arial", 45)
   #Text to show
-  text = "Manchester is a city and metropolitan borough of Greater Manchester, England, which had an estimated population of 568,996 in 2022. Greater Manchester is the third-most populous metropolitan area in the United Kingdom, with a population of 2.92 million, and the largest in Northern England. "
+  text = "Milan is a city in northern Italy, regional capital of Lombardy, the largest city in Italy by urban population and the second-most-populous city proper in Italy after Rome. The city proper has a population of about 1.4 million, while its metropolitan city has 3.25 million residents. The urban area of Milan is the fourth-most-populous in the EU with 6.17 million inhabitants. According to national sources, the population within the wider Milan metropolitan area is estimated between 7.5 million and 8.2 million, making it by far the largest metropolitan area in Italy and one of the largest in the EU. Milan is the economic capital of Italy, one of the economic capitals of Europe and a global financial centre."
   #Get text width and height
   text_width, text_height = font.size(text)
   # Setting the time
@@ -326,7 +344,65 @@ def horizontalScroll():
 
 
 
-#Text scroll from bottom to top
+#Block of text that moves vertically
+def verticalBlock():
+
+  #Create a font
+  font = pygame.font.SysFont("Arial", 45)
+  #Text to show
+  text = "Eclipses only occur when the Sun, Earth, and Moon are all in a straight line. Solar eclipses occur at new moon, when the Moon is between the Sun and Earth. In contrast, lunar eclipses occur at full moon, when Earth is between the Sun and Moon. The apparent size of the Moon is roughly the same as that of the Sun, with both being viewed at close to one-half a degree wide. The Sun is much larger than the Moon, but it is the vastly greater distance that gives it the same apparent size as the much closer and much smaller Moon from the perspective of Earth. The variations in apparent size, due to the non-circular orbits, are nearly the same as well, though occurring in different cycles. This makes possible both total (with the Moon appearing larger than the Sun) and annular (with the Moon appearing smaller than the Sun) solar eclipses.[217] In a total eclipse, the Moon completely covers the disc of the Sun and the solar corona becomes visible to the naked eye."
+  #Get text width and height
+  text_width, text_height = font.size(text)
+  # Setting the time
+  test_time = 15
+  #Number of characters per line
+  n = 30
+  t_end = time.time() + test_time
+
+  #Starting image position and speed
+  x = sizeWidth/2 - (text_width/(n*2))
+  y = sizeHeight
+  speed = 0.35
+  text = addSeparator(text, n)
+  #text with new line
+  nlText = text.split("#")
+
+  '''# File to write on
+  s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
+  file1 = open("C:\\Users\\Davide Mascheroni\\Desktop\\Risultati\\Results{}-Trial{}.txt".format(testernumber, index), "w")'''
+
+  while (x > -text_width) and time.time() <= t_end:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        sys.exit()
+
+    '''# Sending data to the server and writing it on the respective file
+    casual_data = s.recv(1024)
+    file1.write(bytes.decode(casual_data))'''
+
+    y = y - (1*speed)
+    screen.fill(BLACK)
+    createVertBlock(x, y, font, nlText)
+  
+    pygame.display.flip()
+    clock.tick(150)
+
+  '''# Sending data to the server and writing it on the respective file
+  s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="0" />\r\n'))
+  time.sleep(0.3)
+  casual_data = s.recv(1024)
+  time.sleep(0.3)
+  file1.write(bytes.decode(casual_data))
+  file1.write("\n\n Horizontalscroll end \n\n")
+  file1.close()'''
+  time.sleep(0.3)
+
+
+
+
+'''#Text scroll from bottom to top
 def verticalScroll():
 
   #Create a font
@@ -345,20 +421,20 @@ def verticalScroll():
   y = sizeHeight
   speed = 40
 
-  '''# File to write on
+  # File to write on
   s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
-  file1 = open("C:\\Users\\Davide Mascheroni\\Desktop\\Risultati\\Results{}-Trial{}.txt".format(testernumber, index), "w")'''
+  file1 = open("C:\\Users\\Davide Mascheroni\\Desktop\\Risultati\\Results{}-Trial{}.txt".format(testernumber, index), "w")
 
-  while (y > - (text_width + 45*(len(text)-1))) and time.time() <= t_end:
+  while y > - text_width + 45*(len(text)-1) and time.time() <= t_end:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
       if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
         sys.exit()
 
-    '''# Sending data to the server and writing it on the respective file
+    # Sending data to the server and writing it on the respective file
     casual_data = s.recv(1024)
-    file1.write(bytes.decode(casual_data))'''
+    file1.write(bytes.decode(casual_data))
     
     y = y - (1*speed)
 
@@ -370,33 +446,32 @@ def verticalScroll():
     pygame.display.flip()
     clock.tick(150)
 
-  '''# Sending data to the server and writing it on the respective file
+  # Sending data to the server and writing it on the respective file
   s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="0" />\r\n'))
   time.sleep(0.3)
   casual_data = s.recv(1024)
   time.sleep(0.3)
   file1.write(bytes.decode(casual_data))
   file1.write("\n\n Verticalscroll end \n\n")
-  file1.close()'''
+  file1.close()
   time.sleep(0.3)
+  '''
 
-#Block of text that moves vertically
-def verticalBlock():
-  print("ciao")
 
 def main():
     
   pygame.init()
   pygame.mouse.set_visible(False)
 
-  #shuffle the order of the animations
-  tests_list = [horizontalMove, verticalMove, diagMove, horizontalScroll, verticalScroll]
+  '''#shuffle the order of the animations
+  tests_list = [horizontalMove, verticalMove, diagMove, horizontalScroll, verticalBlock]
   random.shuffle(tests_list)
 
   #run the animation after the shuffle
   for funct in tests_list:
-    funct()
+    funct()'''
   
+  diagMove()
 
   pygame.quit()
   '''s.close()'''

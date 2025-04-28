a3943c3d51ae32e10b66557bacc738af967758c9
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-04-28 11:33:51 +0200

    text indent

--- test.py
+++ test.py
@@ -28,10 +28,8 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = '{0},{1}'.format(glb_var_const.win_pos_left
 screen = pygame.display.set_mode(glb_var_const.winsize, pygame.FULLSCREEN)
 pygame.display.set_caption("Test")
 
-    
 # Used to manage how fast the screen updates. Create an object to manage the time
 clock = pygame.time.Clock()
-
 
 '''SERVER CONNECTION'''
 
@@ -57,28 +55,28 @@ s.send(str.encode('<SET ID="ENABLE_SEND_EYE_RIGHT" STATE="1" />\r\n'))
 s.send(str.encode('<SET ID="ENABLE_SEND_BLINK" STATE="1" />\r\n'))'''
 
 
+#Create a white cross to display
 def draw_fixation_cross(x, y, length=20, width=5, color=pygame.Color(glb_var_const.WHITE)):
   pygame.draw.line(screen, color, (x, y - length), (x, y + length), width)
   pygame.draw.line(screen, color, (x - length, y), (x + length, y), width)
 
+
 #Show white cross for tcross seconds
 def show_white_cross():
   t_end = time.time() + glb_var_const.TCROSS
-  
   while time.time() <= t_end:
     screen.fill(pygame.Color(glb_var_const.BLACK)) 
     draw_fixation_cross(glb_var_const.center_x, glb_var_const.center_y)
     pygame.display.flip()  
 
 
-
+#Connect to the database
 def db_connection():
-  
   cnx = mysql.connector.connect(user='root', password='Dadeinter99', host='127.0.0.1', database='indexes')
   return cnx
 
 
-#Insert the new K generated texts
+#Insert K generated texts in the database
 def insert_k_texts(mycursor, text, conn):
   for i, j in zip(range(0,8), text):
     mycursor.execute("INSERT INTO rem_index (tester_number, session_number, trial_number, index_funct, txt) VALUES (%s, %s, %s, %s, %s)", (tester_number, session_number, trial_number, i, j))
@@ -90,22 +88,22 @@ def gen_random_text(allTexts):
   text = random.sample(allTexts, glb_var_const.K)
   return text
 
+
+#Remove previously used texts from the text list
 def rem_used_texts(myresult):
   for j in range(len(myresult)):
     glb_var_const.allTexts.remove(myresult[j][0])
 
 
-#Assign the random text
+#Return fresh random text
 def random_text():
-
   testerN_int = int(tester_number)
   sessionN_int = int(session_number)
   trialN_int = int(trial_number)
-
   #start the db connection
   conn = db_connection()
   mycursor = conn.cursor()
-
+  
   #Check the correct input
   if((sessionN_int > 3 or sessionN_int < 1) or (trialN_int > 3 or trialN_int < 1) or (testerN_int < 1)):
       raise exception.MyException("Invalid input number")
@@ -115,10 +113,8 @@ def random_text():
     #Execute the query
     mycursor.execute("SELECT * FROM rem_index WHERE tester_number = %s", (tester_number, ))
     row = mycursor.fetchone()
-
     if (row == None):
       insert_k_texts(mycursor, text, conn)
-      
     else:
       raise exception.MyException("There are already data for tester {} during session {} and trial {} in the database".format(tester_number, session_number, trial_number))
     
@@ -128,7 +124,6 @@ def random_text():
       mycursor.execute("SELECT txt FROM rem_index WHERE tester_number = %s and trial_number = %s", (tester_number, i))
       myresult = mycursor.fetchall()
       rem_used_texts(myresult)
-    
     text = gen_random_text(glb_var_const.allTexts)
     insert_k_texts(mycursor, text, conn)
 
@@ -138,7 +133,6 @@ def random_text():
       mycursor.execute("SELECT txt FROM rem_index WHERE tester_number = %s and session_number = %s", (tester_number, i))
       myresult = mycursor.fetchall()
       rem_used_texts(myresult)
-
     text = gen_random_text(glb_var_const.allTexts)
     insert_k_texts(mycursor, text, conn)
 
@@ -148,13 +142,11 @@ def random_text():
       mycursor.execute("SELECT txt FROM rem_index WHERE tester_number = %s and session_number = %s", (tester_number, i))
       myresult = mycursor.fetchall()
       rem_used_texts(myresult)
-
     #Remove already used texts in the previous trials
     for i in range(1, trialN_int):
       mycursor.execute("SELECT txt FROM rem_index WHERE tester_number = %s and trial_number = %s and session_number = %s", (tester_number, i, session_number))
       myresult = mycursor.fetchall()
       rem_used_texts(myresult)
-
     text = gen_random_text(glb_var_const.allTexts)
     insert_k_texts(mycursor, text, conn)
 
@@ -167,32 +159,26 @@ def random_text():
 def addSeparator(text, n, max_lenght):
   s = list(text)
   ml = len(text)
-
   if(max_lenght > ml):
     max_lenght = ml
-
   for i in range(max_lenght):
     if(i%n == 0) and i != 0:
       s[i] = '#' + s[i]
-
   text = ''.join(s[0:i])
   return text
 
 
+#Create a vertical block of text
 def createVertBlock(x, y, font, nlText, char_size):
-
   for i in range(len(nlText)):
     img = font.render(nlText[i], True, glb_var_const.WHITE)
     screen.blit(img, (x, y))
     y = y + char_size
 
 
-
 #Text scroll from right to left
 def horizontalScroll(txt, speed, dim_char, fname):
-
   show_white_cross()
-
   #Create a font
   font = pygame.font.SysFont(glb_var_const.FONT, dim_char)
   #Text to show
@@ -200,7 +186,6 @@ def horizontalScroll(txt, speed, dim_char, fname):
   #Get text width and height
   text_width, text_height = font.size(text)
   t_end = time.time() + glb_var_const.TEST_TIME
-
   #Starting image position and speed
   x = glb_var_const.sizeWidth
   y = (glb_var_const.sizeHeight / 2) - (text_height / 2)
@@ -209,6 +194,7 @@ def horizontalScroll(txt, speed, dim_char, fname):
   s.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
   file1 = open("C:\\Users\\Davide Mascheroni\\Desktop\\T{}-S{}-TRY{}-HS_{}.txt".format(tester_number, session_number, trial_number, fname), "w")
   file1.write(str(datetime.datetime.now())+"\n")'''
+
   while (x > -text_width) and time.time() <= t_end:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -241,9 +227,7 @@ def horizontalScroll(txt, speed, dim_char, fname):
 
 #Block of text that moves vertically
 def verticalBlock(txt, speed, dim_char, fname):
-
   show_white_cross()
-
   #Create a font
   font = pygame.font.SysFont(glb_var_const.FONT, dim_char)
   #Text to show
@@ -253,12 +237,10 @@ def verticalBlock(txt, speed, dim_char, fname):
   #Number of characters per line
   n = 30
   t_end = time.time() + glb_var_const.TEST_TIME
-
   text = addSeparator(text, n, len(text))
   #text with new line
   nlText = text.split("#")
   line_width, line_height = font.size(nlText[0])
-   
   #Starting image position and speed
   x = glb_var_const.sizeWidth/2 - (line_width/2)
   y = glb_var_const.sizeHeight
@@ -282,7 +264,6 @@ def verticalBlock(txt, speed, dim_char, fname):
     y = y - (1*speed)
     screen.fill(glb_var_const.BLACK)
     createVertBlock(x, y, font, nlText, dim_char)
-  
     pygame.display.flip()
     clock.tick(150)
 
@@ -335,9 +316,6 @@ def main():
   #run the animation after the shuffle
   for funct, txt in zip(tests_list, text):
     funct(txt)'''
-  
-
-  show_white_cross()
 
   pygame.quit()
   '''s.close()'''

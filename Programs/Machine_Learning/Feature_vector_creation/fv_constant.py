4bdd2608dc936828d44b2df7dddb105b71ab67fb
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-07-04 12:57:56 +0200

    feature vector refactor

---
+++ Programs/Machine_Learning/Feature_vector_creation/fv_constant.py
@@ -0,0 +1,14 @@
+number_of_testers = 35
+
+#Lab Path
+#BASE_PATH = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Results_csv"
+BASE_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Results_csv"
+#Lab Path
+#OUT_PATH = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\feature_vector.csv"
+OUT_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\feature_vector2.csv"
+#Lab Path
+#FIX_VECTOR_PATH = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\fixation_vector.csv"
+FIX_VECTOR_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector2.csv"
+
+filenames = ["SL_LIT", "SL_BIG", "FA_LIT", "FA_BIG"]
+animnames = ["VB", "HS"]

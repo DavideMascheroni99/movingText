4c7d5a2ed63dd1300c4f7ca631cd55689863c130
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-07-09 18:58:41 +0200

    identification

--- Programs/Machine_Learning/Identification.py
+++ Programs/Machine_Learning/Identification.py
@@ -21,8 +21,8 @@ warnings.filterwarnings(
 
 '''LOAD THE DATASET'''
 #csv_path of the PC in the lab
-csv_path = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\feature_vector.csv" 
-#csv_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\feature_vector.csv"
+#csv_path = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\feature_vector.csv" 
+csv_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\feature_vector.csv"
 dataset = pd.read_csv(csv_path)
 
 #Extract the tester id and the session id from file_key
@@ -32,12 +32,6 @@ dataset['session_id'] = dataset['file_key'].apply(lambda x: x.split('_')[1])
 #The X are the whole rows composed by the whole columns, while the labels are the id of the persons
 X = dataset.loc[:, 'f0':'f82']
 y = dataset['person_id']
-
-'''DIFFERENT SPLIT OF THE DATA'''
-# Random split (80/20) with stratification
-X_train_rand, X_test_rand, y_train_rand, y_test_rand = train_test_split(
-    X, y, test_size=0.2, random_state=0, stratify=y
-)
 
 # Session split (S1+S2 for training, S3 for test)
 train_subset = dataset[dataset['session_id'].isin(['S1', 'S2'])]
@@ -144,7 +138,7 @@ def get_mlp_pipeline():
 
 '''GRID SEARCH FUNCTION'''
 
-def run_grid_search(X_train, y_train, X_test, y_test, pipeline, param_grid, title, results_path=None):
+def run_grid_search(X_train, y_train, X_test, y_test, pipeline, param_grid, title):
     print(f"\n=== {title} ===")
     grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='accuracy', n_jobs=-1, verbose=0)
     grid_search.fit(X_train, y_train)
@@ -158,7 +152,10 @@ def run_grid_search(X_train, y_train, X_test, y_test, pipeline, param_grid, titl
     print("Best CV accuracy:", best_cv_score)
     print("Train accuracy:", train_score)
     print("Test accuracy:", test_score)
-    
+
+    return title, best_params, best_cv_score, train_score, test_score
+
+def write_results(title, best_params, best_cv_score, train_score, test_score, results_path):
     if results_path:
         results = {
             'Model': title,
@@ -187,19 +184,30 @@ model_list = [
 ]
 
 #Result file path
-results_file = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Programs\Machine_Learning\Machine_Learning_results\Identification_results.csv" 
-#results_file = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Programs\Machine_Learning\Machine_Learning_results\Identification_results.csv"
+#results_file = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Programs\Machine_Learning\Machine_Learning_results\Identification_results.csv" 
+results_file = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Programs\Machine_Learning\Machine_Learning_results\Identification_results.csv"
 
 #If i rerun the code I want to delete the previous results file
 if os.path.exists(results_file):
     os.remove(results_file)  
 
-# 1. Random Split (80/20)
-for model_name, model_fn in model_list:
-    pipeline, param_grid = model_fn()
-    run_grid_search(X_train_rand, y_train_rand, X_test_rand, y_test_rand, pipeline, param_grid, model_name + " (80/20)", results_path=results_file)
+
+
+for i in range(10):
+    # Random split (80/20) with stratification
+    X_train_rand, X_test_rand, y_train_rand, y_test_rand = train_test_split(
+        X, y, test_size=0.2, random_state=i, stratify=y
+    )
+    # 1. Random Split (80/20)
+    for model_name, model_fn in model_list:
+        pipeline, param_grid = model_fn()
+        title, best_params, best_cv_score, train_score, test_score = run_grid_search(X_train_rand, y_train_rand, X_test_rand, y_test_rand, pipeline, param_grid, model_name + " (80/20)")
+    
+write_results(title, best_params, best_cv_score, train_score, test_score, results_file)
+
 
 # 2. Session Split (S1+S2 → train, S3 → test)
 for model_name, model_fn in model_list:
     pipeline, param_grid = model_fn()
-    run_grid_search(X_train_sess, y_train_sess, X_test_sess, y_test_sess, pipeline, param_grid, model_name + " (S1+S2 vs S3)", results_path=results_file)
+    title, best_params, best_cv_score, train_score, test_score = run_grid_search(X_train_sess, y_train_sess, X_test_sess, y_test_sess, pipeline, param_grid, model_name + " (S1+S2 vs S3)")
+    write_results(title, best_params, best_cv_score, train_score, test_score, results_file) 

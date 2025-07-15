c019d75f61a4080fbb54252bb48a5d03fcd7844b
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-07-15 10:46:56 +0200

    new feature vector

--- Programs/Machine_Learning/Feature_vector_creation/fv_constant.py
+++ Programs/Machine_Learning/Feature_vector_creation/fv_constant.py
@@ -5,10 +5,10 @@ number_of_testers = 35
 BASE_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Results_csv"
 #Lab Path
 #OUT_PATH = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\feature_vector.csv"
-OUT_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\feature_vector2.csv"
+OUT_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\feature_vector.csv"
 #Lab Path
 #FIX_VECTOR_PATH = r"C:\Users\Davide Mascheroni\Desktop\movingText\movingText\Feature_csv\fixation_vector.csv"
-FIX_VECTOR_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector2.csv"
+FIX_VECTOR_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector.csv"
 
 filenames = ["SL_LIT", "SL_BIG", "FA_LIT", "FA_BIG"]
 animnames = ["VB", "HS"]

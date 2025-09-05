bc927d27bc674ca8f5feba25ff19aa1ebaa3f7e7
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-09-05 11:57:39 +0200

    fix

--- Programs/Feature_vector_creation/fv_constant.py
+++ Programs/Feature_vector_creation/fv_constant.py
@@ -10,8 +10,8 @@ TEN_OUT_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Featur
 
 # Fixation vector path where to save the result (complete - only first 5 seconds - only first 10 seconds)
 FIX_VECTOR_PATH = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector.csv"
-FIX_VECTOR_PATH_FIVE = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector_5sec.csv"
-FIX_VECTOR_PATH_TEN = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\fixation_vector_10sec.csv"
+FIX_VECTOR_PATH_FIVE = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\five_sec_ds\fixation_vector_5sec.csv"
+FIX_VECTOR_PATH_TEN = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Feature_csv\ten_sec_ds\fixation_vector_10sec.csv"
 
 filenames = ["SL_LIT", "SL_BIG", "FA_LIT", "FA_BIG"]
 animnames = ["VB", "HS"]

077e28af0131fb435f36b8f4074519a8e6877426
DavidePortatile <97758477+DavideMascheroni1@users.noreply.github.com>
2025-09-13 12:38:47 +0200

    fix

--- Programs/Table_creation/table_creator_ver_single.py
+++ Programs/Table_creation/table_creator_ver_single.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # --- User-defined paths ---
-input_csv_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Programs\Machine_Learning\Machine_Learning_results\Verification_single_intruders_results\Verification_single_intruders_results_ft.csv"
-output_ods_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Graphs\Verification_single_intruders\Verification_single_intruders_ft.ods"
+input_csv_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Programs\Programs\Machine_Learning\Machine_Learning_results\Verification_single_five_ten\Verification_single_ten_results_st.csv"
+output_ods_path = r"C:\Users\david\OneDrive\Documenti\Tesi_BehavBio\Graphs\Verification_five_ten\Verification_single_ten_st.ods"
 
 # Step 1: Read CSV
 df = pd.read_csv(input_csv_path)
